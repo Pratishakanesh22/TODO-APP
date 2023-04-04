c04add50447a9da3aca4b4076dc1b749f0aaeae5
Hariom Joshi <hariomjoshi331@gmail.com>
2023-04-04 12:50:19 +0530

    Connecting to MongoDB

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes/TODORoute')
+const routes = require('./backend/routes/TODORoute')
 //require('dotenv').config()
 //const cors = require("cors")
 const app = express()
-const PORT = process.env.port || 5000
+const PORT = 5000
  
 //app.use(express.json());
 //app.use(cors());
-
+const Mongo_URL="mongodb+srv://pratisha:pratisha2208@cluster0.3mocm64.mongodb.net/TODO-APP?retryWrites=true&w=majority"
 mongoose
-    .connect("MONGODB_URL = mongodb+srv://pratisha:pratisha2208@cluster0.3mocm64.mongodb.net/TODO-APP?retryWrites=true&w=majority")
+    .connect(Mongo_URL)
          
     .then(() => console.log(' Connected To MongoDB...'))
     .catch((err) => console.error(err))
@@ -18,4 +18,4 @@ mongoose
  //routes
 app.use(routes)
 
-app.listen(PORT, () => console.log('Listening on: ${PORT}'))
+app.listen(PORT, () => console.log(`Listening on: ${PORT}`))
